2c75ac653c135497113c1ed88246dea633754dde
Roboguy99 <Roboguy99@gmail.com>
2015-06-20 11:06:56 +0100

    Removed old background methods.

--- menu/Menu.java
+++ menu/Menu.java
@@ -1,13 +1,5 @@
 package menu;
 
-import static org.lwjgl.opengl.GL11.GL_LINEAR;
-import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glTexCoord2f;
-import static org.lwjgl.opengl.GL11.glVertex2f;
 import gui.components.button.Button;
 import gui.components.button.ButtonLoadGame;
 import gui.components.button.ButtonNewGame;
@@ -20,7 +12,6 @@ import java.io.IOException;
 import main.Main;
 
 import org.newdawn.slick.Color;
-import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
 
@@ -57,15 +48,12 @@ public class Menu
 	//Create other things
 	private Label lblVersion = new Label(20, Metadata.VERSION);
 	
-	private Texture texture; //Background texture
-	
 	
 	public Menu()
 	{
 		try //Attempt to load the background texture
 		{
 			Main.menuTexture = TextureLoader.getTexture("JPG", ResourceLoader.getResourceAsStream("assets/textures/terrain/Earth_Terrain_1080.jpg"));
-			texture = Main.menuTexture; //Update the main texture
 		} 
 		catch (IOException e) 
 		{
@@ -74,9 +62,7 @@ public class Menu
 	}
 	
 	public void draw()
-	{
-		drawBackground(); //Draw the background
-		
+	{	
 		//Draw the menu buttons
 		Color.white.bind();
 		btnNewGame.draw(20, 20, BUTTON_LENGTH, BUTTON_HEIGHT);
@@ -86,26 +72,5 @@ public class Menu
 		
 		//Draw other things
 		lblVersion.draw(10, Main.WINDOW_HEIGHT - 30, Color.black);
-	}
-	
-	private void drawBackground()
-	{
-		texture = Main.menuTexture;
-		
-		glPushMatrix();
-			texture.bind();
-			Color.white.bind();
-			texture.setTextureFilter(GL_LINEAR);
-			glBegin(GL_QUADS);
-				glTexCoord2f(0,0);
-				glVertex2f(0,0);
-				glTexCoord2f(1,0);
-				glVertex2f(texture.getTextureWidth(),0);
-				glTexCoord2f(1,1);
-				glVertex2f(texture.getTextureWidth(),texture.getTextureHeight());
-				glTexCoord2f(0,1);
-				glVertex2f(0,texture.getTextureHeight());	
-	        glEnd();
-        glPopMatrix();
 	}
 }
