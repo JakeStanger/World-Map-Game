5623b551c5ae784de0a5a0bddbf707d6558f1f2b
Roboguy99 <Roboguy99@gmail.com>
2015-06-20 11:04:42 +0100

    Removed old background drawing methods

--- game/Game.java
+++ game/Game.java
@@ -1,18 +1,6 @@
 package game;
 
-import static org.lwjgl.opengl.GL11.GL_LINEAR;
-import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glTexCoord2f;
-import static org.lwjgl.opengl.GL11.glVertex2f;
 import main.Main;
-
-import org.newdawn.slick.Color;
-import org.newdawn.slick.opengl.Texture;
-
 import util.Generator;
 import util.InstanceManager;
 
@@ -33,14 +21,10 @@ public class Game
 	
 	public static String START_COUNTRY, START_CITY, END_COUNTRY, END_CITY; //Blank to avoid null pointer exception
 	
-	/**
-	 * The map texture
-	 */
-	private Texture texture;
-	
 	public Game()
 	{
 		this.clock = new Clock(generator);
+		this.instanceManager.intro = true;
 	}
 	
 	public void tick()
@@ -64,30 +48,6 @@ public class Game
 	 */
 	private void drawGame()
 	{
-		drawBackground();
-	}
-	
-	/**
-	 * Draw the background as a static map
-	 */
-	private void drawBackground()
-	{
-		texture = Main.menuTexture;
-		
-		glPushMatrix();
-			texture.bind();
-			Color.white.bind();
-			texture.setTextureFilter(GL_LINEAR);
-			glBegin(GL_QUADS);
-				glTexCoord2f(0,0);
-				glVertex2f(0,0);
-				glTexCoord2f(1,0);
-				glVertex2f(texture.getTextureWidth(),0);
-				glTexCoord2f(1,1);
-				glVertex2f(texture.getTextureWidth(),texture.getTextureHeight());
-				glTexCoord2f(0,1);
-				glVertex2f(0,texture.getTextureHeight());	
-	        glEnd();
-        glPopMatrix();
+		//drawBackground();
 	}
 }
--- menu/options/Options.java
+++ menu/options/Options.java
@@ -1,22 +1,13 @@
 package menu.options;
 
-import static org.lwjgl.opengl.GL11.GL_LINEAR;
-import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glTexCoord2f;
-import static org.lwjgl.opengl.GL11.glVertex2f;
-import main.Main;
 import gui.components.button.Button;
 import gui.components.button.options.ButtonMenuReturn;
 import gui.components.button.options.ButtonTerrainMap;
 import gui.components.button.options.ButtonWhiteMap;
 import gui.components.label.Label;
+import main.Main;
 
 import org.newdawn.slick.Color;
-import org.newdawn.slick.opengl.Texture;
 
 
 public class Options 
@@ -41,43 +32,18 @@ public class Options
 	
 	private Label lblPaused = new Label(30, "Paused");
 	
-	private Texture texture;
-	
 	public Options()
 	{
 		//Constructor code goes here
 	}
 	
 	public void draw(boolean inGame)
-	{
-		drawBackground();
-		
+	{	
 		Color.white.bind();
 		
 		btnTerrain.draw(20, 20, BUTTON_LENGTH, BUTTON_HEIGHT);
 		btnWhite.draw(20, BUTTON_HEIGHT + 40, BUTTON_LENGTH, BUTTON_HEIGHT);
 		if (!inGame) btnBack.draw(20, BUTTON_HEIGHT*2 + 60, BUTTON_LENGTH, BUTTON_HEIGHT);
 		else lblPaused.draw(Main.WINDOW_WIDTH-lblPaused.getLength()*lblPaused.size-20, Main.WINDOW_HEIGHT-50, Color.darkGray);
-	}
-	
-	private void drawBackground()
-	{
-		texture = Main.menuTexture;
-		
-		glPushMatrix();
-			if(texture != null) texture.bind();
-			Color.white.bind();
-			texture.setTextureFilter(GL_LINEAR);
-			glBegin(GL_QUADS);
-				glTexCoord2f(0, 0);
-				glVertex2f(0, 0);
-				glTexCoord2f(1, 0);
-				glVertex2f(texture.getTextureWidth(), 0);
-				glTexCoord2f(1, 1);
-				glVertex2f(texture.getTextureWidth(), texture.getTextureHeight());
-				glTexCoord2f(0, 1);
-				glVertex2f(0, texture.getTextureHeight());	
-	        glEnd();
-        glPopMatrix();
 	}
 }
