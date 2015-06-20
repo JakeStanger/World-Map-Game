78eb72b907801141e67097ae5c873cbe53bf9702
Roboguy99 <Roboguy99@gmail.com>
2015-06-20 11:06:08 +0100

    Whoops - wrong class!

--- game/Game.java
+++ game/Game.java
@@ -1,6 +1,18 @@
 package game;
 
+import static org.lwjgl.opengl.GL11.GL_LINEAR;
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glPopMatrix;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glVertex2f;
 import main.Main;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.Texture;
+
 import util.Generator;
 import util.InstanceManager;
 
@@ -20,6 +32,11 @@ public class Game
 	private InstanceManager instanceManager = Main.instanceManager;
 	
 	public static String START_COUNTRY, START_CITY, END_COUNTRY, END_CITY; //Blank to avoid null pointer exception
+	
+	/**
+	 * The map texture
+	 */
+	private Texture texture;
 	
 	public Game()
 	{
@@ -48,6 +65,30 @@ public class Game
 	 */
 	private void drawGame()
 	{
-		//drawBackground();
+		drawBackground();
+	}
+	
+	/**
+	 * Draw the background as a static map
+	 */
+	private void drawBackground()
+	{
+		texture = Main.menuTexture;
+		
+		glPushMatrix();
+			texture.bind();
+			Color.white.bind();
+			texture.setTextureFilter(GL_LINEAR);
+			glBegin(GL_QUADS);
+				glTexCoord2f(0,0);
+				glVertex2f(0,0);
+				glTexCoord2f(1,0);
+				glVertex2f(texture.getTextureWidth(),0);
+				glTexCoord2f(1,1);
+				glVertex2f(texture.getTextureWidth(),texture.getTextureHeight());
+				glTexCoord2f(0,1);
+				glVertex2f(0,texture.getTextureHeight());	
+	        glEnd();
+        glPopMatrix();
 	}
 }
