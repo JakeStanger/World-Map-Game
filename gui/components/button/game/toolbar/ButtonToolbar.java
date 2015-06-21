9817db33dcb78ce5d6e6624aace6e8c6cc1f5d69
Roboguy99 <Roboguy99@gmail.com>
2015-06-21 19:31:04 +0100

    Created parent for all toolbar buttons

---
+++ gui/components/button/game/toolbar/ButtonToolbar.java
@@ -0,0 +1,17 @@
+package gui.components.button.game.toolbar;
+
+import gui.components.button.Button;
+
+public class ButtonToolbar extends Button
+{
+	public ButtonToolbar(int height, String text) 
+	{
+		super(height, text);
+	}
+
+	@Override
+	protected void click()
+	{
+		
+	}
+}
