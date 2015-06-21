c7447afe27cf41eee0ec5f9c2ecbe1d6f78c327b
Roboguy99 <Roboguy99@gmail.com>
2015-06-21 19:31:21 +0100

    Created plane button

---
+++ gui/components/button/game/toolbar/ButtonPlane.java
@@ -0,0 +1,17 @@
+package gui.components.button.game.toolbar;
+
+import gui.components.button.Button;
+
+public class ButtonPlane extends Button
+{
+	public ButtonPlane(int height, String text)
+	{
+		super(height, text);
+	}
+
+	@Override
+	protected void click()
+	{
+		
+	}
+}
