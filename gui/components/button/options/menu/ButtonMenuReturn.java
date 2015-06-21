57ee531502c53f0fc48c7e58b21acbfd076d82d0
Roboguy99 <Roboguy99@gmail.com>
2015-06-21 14:08:39 +0100

    Moved to menu specific package.

---
+++ gui/components/button/options/menu/ButtonMenuReturn.java
@@ -0,0 +1,30 @@
+package gui.components.button.options.menu;
+
+import gui.components.button.Button;
+import main.Main;
+
+public class ButtonMenuReturn extends Button 
+{
+	public ButtonMenuReturn(int height, String text) 
+	{
+		super(height, text);
+	}
+
+	@Override
+	protected void click() 
+	{
+		if(Main.instanceManager.menuInstance == null)
+		{
+			Main.instanceManager.updateMenu(true);
+		}
+		
+		//Begin drawing the correct components and stop the unneeded ones
+		Main.instanceManager.optionsInstance.draw = false;
+		Main.instanceManager.menuInstance.draw = true;
+		
+		if(Main.instanceManager.optionsInstance != null)
+		{
+			Main.instanceManager.updateOptions(false);
+		}
+	}
+}
