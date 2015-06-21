cf21f2e036b1230d1c9bc37f9d24ac5f29afeb90
Roboguy99 <Roboguy99@gmail.com>
2015-06-21 14:00:28 +0100

    Switched new game class for intro class again

--- gui/components/button/ButtonNewGame.java
+++ gui/components/button/ButtonNewGame.java
@@ -12,15 +12,17 @@ public class ButtonNewGame extends Button
 	@Override
 	protected void click() 
 	{
-		if(Main.instanceManager.newGameInstance == null) 
+		//Begin drawing the game instead of the main menu
+		if(Main.instanceManager.introInstance == null)
 		{
-			Main.instanceManager.updateNewGame(true);
+			Main.instanceManager.updateIntro(true);
 		}
 		
 		//Begin drawing the correct components and stop the unneeded ones
 		Main.instanceManager.menuInstance.draw = false;
+		Main.instanceManager.introInstance.draw = true;
 		
-		if(Main.instanceManager.menuInstance != null)
+		if(Main.instanceManager.menuInstance != null) 
 		{
 			Main.instanceManager.updateMenu(false);
 		}
