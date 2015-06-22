4af1c31bc84753f7a09d5aef94ac3da335d4c64c
Roboguy99 <Roboguy99@gmail.com>
2015-06-22 19:50:34 +0100

    Added click functionality (panel toggle)

--- gui/components/button/game/toolbar/ButtonPlane.java
+++ gui/components/button/game/toolbar/ButtonPlane.java
@@ -1,6 +1,8 @@
 package gui.components.button.game.toolbar;
 
 import gui.components.button.ButtonImage;
+import gui.components.panel.toolbar.PanelPlane;
+import main.Main;
 import util.ResourceManager;
 
 public class ButtonPlane extends ButtonImage
@@ -13,6 +15,9 @@ public class ButtonPlane extends ButtonImage
 	@Override
 	protected void click()
 	{
+		PanelPlane pnlPlane = Main.instanceManager.gameInstance.guiEngine.pnlPlane;
 		
+		if(pnlPlane.draw) pnlPlane.draw = false;
+		else pnlPlane.draw = true;
 	}
 }
