eb796c3a452ba61442e6f48bafa2304787827445
Roboguy99 <Roboguy99@gmail.com>
2015-06-22 19:42:13 +0100

    Created panel test class.

---
+++ gui/components/panel/toolbar/PanelPlane.java
@@ -0,0 +1,12 @@
+package gui.components.panel.toolbar;
+
+import util.ResourceManager;
+import gui.components.panel.Panel;
+
+public class PanelPlane extends Panel
+{
+	public PanelPlane()
+	{
+		super(ResourceManager.TOOLBAR); //TODO Update placeholder texture;
+	}
+}
