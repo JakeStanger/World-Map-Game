6fa44a5c9337afe0db1e7864803fa6354e249f0c
Roboguy99 <Roboguy99@gmail.com>
2015-06-22 19:41:51 +0100

    Updated to use images instead of blank box.

--- gui/components/panel/Panel.java
+++ gui/components/panel/Panel.java
@@ -1,46 +1,57 @@
 package gui.components.panel;
 
+import static org.lwjgl.opengl.GL11.GL_LINEAR;
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glColor4d;
 import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glPopMatrix;
 import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glTranslated;
-import static org.lwjgl.opengl.GL11.glVertex2d;
-import gui.Component;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glTranslatef;
+import static org.lwjgl.opengl.GL11.glVertex2f;
+import main.Main;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.Texture;
 
 /**
- * A GUI panel
+ * A pop-out panel, normally created on button click
  * @author Jake
  *
  */
-public class Panel extends Component
+public class Panel
 {
+	private Texture texture;
+	
+	public Panel(Texture texture)
+	{
+		this.texture = texture;
+	}
+	
 	/**
-	 * Draw the panel
-	 * @param x the start x
-	 * @param y the start y
-	 * @param width the panel width
-	 * @param height the panel height
+	 * Draw the toolbar at the bottom of the screen.
 	 */
-	public void draw(int x, int y, int width, int height, Texture texture)
+	public void draw(int x, int y, int width)
 	{
 		glPushMatrix();
-			glTranslated(x, y, 0);
+			glTranslatef(0F, (float)Main.WINDOW_HEIGHT-texture.getTextureHeight(), 0F);
 			
+			if(texture != null) texture.bind();
+			Color.lightGray.bind();
+			texture.setTextureFilter(GL_LINEAR);
 			glBegin(GL_QUADS);
-				glColor4d(1, 1, 1, 1); //Texture fallback
-				glVertex2d(0, 0);
-				
-				glVertex2d(width, 0);
-				
-				glVertex2d(width, height);
-				
-				glVertex2d(0, height);
-			glEnd();
-		glPopMatrix();
+			    glTexCoord2f(0, 0);  //top left
+			    glVertex2f(0, 0);
+	
+			    glTexCoord2f(texture.getWidth(),0);    //Top right
+			    glVertex2f(texture.getTextureWidth(), 0);
+	
+			    glTexCoord2f(texture.getWidth(), texture.getHeight()); //Bottom right
+			    glVertex2f(texture.getTextureWidth(), texture.getTextureHeight());
+	
+			    glTexCoord2f(0, texture.getHeight()); //bottom left
+			    glVertex2f(0, texture.getTextureHeight());
+		    glEnd();
+        glPopMatrix();
 	}
 }
