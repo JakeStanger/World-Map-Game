a426ec2c2d8e79715d11b08a5becb285a69dab1e
Roboguy99 <Roboguy99@gmail.com>
2015-06-22 19:50:51 +0100

    Draw plane panel if draw = true

--- gui/GuiEngine.java
+++ gui/GuiEngine.java
@@ -1,18 +1,24 @@
 package gui;
 
 import gui.components.Toolbar;
+import gui.components.panel.toolbar.PanelPlane;
 
 public class GuiEngine
 {
 	private Toolbar toolbar;
 	
+	public PanelPlane pnlPlane;
+	
 	public GuiEngine()
 	{
 		this.toolbar = new Toolbar();
+		
+		this.pnlPlane = new PanelPlane();
 	}
 	
 	public void draw()
 	{
 		toolbar.draw();
+		if(pnlPlane.draw) pnlPlane.draw(100, 100);
 	}
 }
